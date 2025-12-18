008f34e98959a72040442abf96261684b80aa247
Lavanya Sai Kumar Kantubhukta <65954982+luvsai@users.noreply.github.com>
2025-12-18 20:20:05 +0530

    Create 111-MinimumDepthOfBinaryTree

---
+++ 111-MinimumDepthOfBinaryTree/111-MinimumDepthOfBinaryTree.py
@@ -0,0 +1,23 @@
+# Last updated: 18/12/2025, 20:20:05
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        q = deque()
+        q.append((root, 1))
+        while q:
+            node, depth = q.popleft()
+
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                q.append((node.left, depth + 1))
+            if node.right:
+                q.append((node.right , depth + 1))
+        return 0
